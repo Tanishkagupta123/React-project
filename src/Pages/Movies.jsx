e3d30025b0fd5e089202938759c9e0a1fdca9a0b
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-01-31 01:49:10 +0530

    first commit

--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -308,13 +308,13 @@ const Home = () => {
 
         </div>
       </div>
-{/* FOOTER */}
+
 <div className="bg-[#0f0f0f] text-gray-300 mt-20">
   
-  {/* TOP */}
+  
   <div className="max-w-7xl mx-auto px-6 py-14 grid grid-cols-1 md:grid-cols-3 gap-10">
 
-    {/* BRAND */}
+
     <div>
       <h2 className="text-3xl font-bold text-white mb-3">Flickster</h2>
       <p className="text-sm text-gray-400 leading-relaxed">
