ec646b647a0414610a84a7543bc4d0870d987210
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-05-06 23:58:39 +0530

    update

--- src/Component/Routing.jsx
+++ src/Component/Routing.jsx
@@ -1,65 +1,42 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar'
-import Foryou from '../Pages/Foryou'
 import Dining from '../Pages/Dining'
 import Events from '../Pages/Events'
 import Movies from '../Pages/Movies'
 import Stores from '../Pages/Stores'
-import Play from '../Pages/Play'
-import Activites from '../Pages/Activites'
 import Signup from '../Main pages/Signup'
 import Login from '../Main pages/Login'
 import Bookticket from "../Pages/Bookticket"
 import SeatLayout from "../Pages/SeatLayout"
 import MyBookings from '../Pages/Mybookings'
 
-
-
-
-
 const Routing = () => {
   return (
-    <>
+    <Routes>
 
-      <Routes>
-        <Route path="/" element={<Navbar />}>
+      {/* Layout Route */}
+      <Route path="/" element={<Navbar />}>
 
-          <Route index element={<Movies />} />
+        <Route index element={<Movies />} />
 
+        <Route path="dining" element={<Dining />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="events" element={<Events />} />
+        <Route path="stores" element={<Stores />} />
 
-          {/* <Route path='foryou' element={<Foryou/>}/> */}
-          <Route path='dining' element={<Dining />} />
-          <Route path='movies' element={<Movies />} />
-          <Route path='events' element={<Events />} />
-          <Route path='stores' element={<Stores />} />
-          {/* <Route path='activities' element={<Activites/>} /> */}
-          {/* <Route path='play' element={<Play/>}/> */}
-          <Route path="book/:movieName" element={<Bookticket />} />
+        {/* Dynamic Routes */}
+        <Route path="book/:movieName" element={<Bookticket />} />
+        <Route path="seats/:movie/:cinema/:time" element={<SeatLayout />} />
+        <Route path="mybookings" element={<MyBookings />} />
 
-          <Route
-            path="/seats/:movie/:cinema/:time"
-            element={<SeatLayout />}
-          />
+      </Route>
 
-          <Route path="/mybookings" element={<MyBookings />} />
-          {/* <Route
-            path="/booking-success/:id"
-            element={<BookingSuccess />}
-          /> */}
+      {/* Auth Routes */}
+      <Route path="signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
 
-
-
-
-        </Route>
-
-        <Route path='signup' element={<Signup />} />
-        <Route path='login' element={<Login />} />
-
-
-      </Routes>
-
-    </>
+    </Routes>
   )
 }
 
